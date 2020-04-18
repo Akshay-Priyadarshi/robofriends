2cc77fd03248b5e4d8be50072199aa18264cc9b6
Akshay-Priyadarshi <akshayp1904@outlook.com>
2020-04-18 16:28:01 +0530

    Updates

---
+++ precache-manifest.ac8762ce8a69b9953e35b0f82f4b972f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598ca0990a0f9fa51acccd8ddabcd479",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "4fddf7999ff27c2a56eb",
+    "url": "/robofriends/static/css/main.f9b73bb0.chunk.css"
+  },
+  {
+    "revision": "4365d0075be0241d6d3a",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.84d6e4e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fddf7999ff27c2a56eb",
+    "url": "/robofriends/static/js/main.e0ea596c.chunk.js"
+  },
+  {
+    "revision": "8adbf24e3312ffc2ae3e",
+    "url": "/robofriends/static/js/runtime-main.403370f6.js"
+  }
+]);
